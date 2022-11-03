60cfdbcbc3cdfbaaed8b71991b34ab4aec46eebb
Dancamon <117337175+Dancamon@users.noreply.github.com>
2022-11-03 09:12:00 -0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.header');
+ScrollReveal().reveal('.container', { delay: 500 });
+ScrollReveal().reveal('.banner', { delay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
